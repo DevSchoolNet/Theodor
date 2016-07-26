b5a4b8ec22f2cb0115f5783def39076031853010
cautisanu31 <cautisanutheodor@gmail.com>
2016-07-26 15:28:37 +0300

    Temaa1

---
+++ Temaa1/Temaa1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Tema1
+{
+    public class Comenzi
+    {
+        private string denumire { get; set; } = "Ipad Mini";
+        public string tip { get; set; }
+        public int pret { get; set; }
+        public int cantitate { get; set; }
+
+        public Comenzi()
+        {
+            this.tip = "Tableta";
+            this.pret = 1800;
+            this.cantitate = 1;
+        }
+
+        public Comenzi(string denumire, string tip, int pret, int cantitate)
+        {
+            this.denumire = denumire;
+            this.tip = tip;
+            this.pret = pret;
+            this.cantitate = cantitate;
+        }
+
+        // public string Name
+        //{
+        //    get { return nume; }
+        //    set { nume = value; }
+        //}
+
+        public void afisare() => Console.WriteLine("Denumire: " + denumire + ", Tip: " + tip + ", Pret: " + pret + ", Cantitate: " + cantitate);
+        //{
+        //    Console.WriteLine("Denumire Produs: " + denumire);
+        //    Console.WriteLine("Tip Produs: " + tip);
+        //    Console.WriteLine("Pret Produs: " + pret);
+        //    Console.WriteLine("Numarul de produse: " + cantitate);
+        //}
+
+        public int Valoarea()
+        {
+            return this.pret * this.cantitate;
+        }
+
+
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comenzi c1 = new Comenzi();
+            Comenzi c2 = new Comenzi("Galaxy S5", "Smartphone", 1200, 2);
+            c1.afisare();
+            Console.WriteLine("Valoarea comenzii este: " + c1.Valoarea());
+            Console.WriteLine();
+            c2.afisare();
+            Console.WriteLine("Valoarea comenzii este: " + c2.Valoarea());
+
+
+            int Total = c1.Valoarea() + c2.Valoarea();
+            Console.WriteLine();
+            Console.WriteLine($"Suma totala a comenzilor este: {c1.Valoarea()} + {c2.Valoarea()} = {Total} ");
+
+            Console.ReadKey();
+        }
+    }
+}
--- Temaa1/Temaa1/Properties/AssemblyInfo.cs
+++ Temaa1/Temaa1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloProiect")]
+[assembly: AssemblyTitle("Temaa1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloProiect")]
+[assembly: AssemblyProduct("Temaa1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("646fe11e-0c00-4709-abbf-dbc575dce827")]
+[assembly: Guid("0a6154be-c2e6-4e75-9d5d-e912542c61b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
