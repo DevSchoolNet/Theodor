86db62b59ab28aafb58aae26773abba1f651f8f3
cautisanu31 <cautisanutheodor@gmail.com>
2016-07-28 00:04:54 +0300

    TemaOOP

---
+++ Curs3/Curs3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassOop;
+
+namespace Curs3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+          
+        }
+    }
+}
